74bb9be4c196bc0b111191854bc8e4fac5d9e997
Luca Bösch <luca.boesch@bfh.ch>
2024-04-22 18:17:36 +0200

    Prepare for Moodle 4.4.

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024021600;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024042200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022112808;        // Requires this Moodle version (4.1).
 $plugin->component = 'datafield_regex'; // Full name of the plugin (used for diagnostics).
-$plugin->release   = '1.2';             // Readable version of this plugin.
+$plugin->release   = '1.3';             // Readable version of this plugin.
 $plugin->maturity  = MATURITY_STABLE;   // Maturity of this plugin.
